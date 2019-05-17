5da942dd1f1445a270a3f4bb193641fdeeb519f5
fsociety <fsociety@fsociety-PC>
2019-05-17 18:57:08 -0500

    implementation of test

---
+++ Planet_Express/src/test/Planet_Express_test.java
@@ -0,0 +1,47 @@
+package test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import graph.Graph;
+import modelo.Location;
+
+public class Planet_Express_test {
+	
+	private Graph<Location> graph;
+	
+	public void stage(){
+		graph = new Graph<>(false);
+	}
+	
+	@Test
+	public void sceneOne(){
+		stage();
+	}
+	
+	@Test
+	public void sceneTwo(){
+		stage();
+	}
+	
+	@Test
+	public void scenethree(){
+		stage();
+	}
+	@Test
+	public void sceneFour(){
+		stage();
+	}
+	@Test
+	public void sceneFive(){
+		stage();
+	}
+	@Test
+	public void sceneSix(){
+		stage();
+	}
+	@Test
+	public void sceneSeven(){
+		stage();
+	}
+
+}
